5d72b2ece10b49069850d2c032aea92db64b0f09
hitsuyo <nguyentansang3417@gmail.com>
2018-09-10 17:10:14 +0700

    mytheme 9-10-2018

---
+++ demo_page.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+Template Name: demo
+
+http://demo.smnet.vn/quangcaotaxi/wp-admin/customize.php?url=http%3A%2F%2Fdemo.smnet.vn%2Fquangcaotaxi%2F
+
+*/
+
+?>
+<?php require get_template_directory() . '/header_demo.php'; ?>
